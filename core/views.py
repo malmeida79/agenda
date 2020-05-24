d14ce9cdcf3ae7229b95570260cd36ab0f459721
mdalmeida79 <marcos.almeida@taking.com.br>
2020-05-24 17:14:07 -0300

    criados telas de listagem, templates e etc

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from core.models import Evento
 
-# Create your views here.
+
+def lista_eventos(request):
+    # pra um apenas ou select usuario
+    # evento = Evento.objects.get(id=1)
+    # para listar todos
+    # evento = Evento.objects.all()
+
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos':evento}
+    return render(request, 'agenda.html', dados)
