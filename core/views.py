1e5e7463bda074b6d028759f2d9253c4a34a8c86
mdalmeida79 <marcos.almeida@taking.com.br>
2020-05-24 19:13:02 -0300

    adicionados login, logout e mensagens

--- core/views.py
+++ core/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username,password=password)
+        if usuario is not None:
+            login(request, usuario)
+        else:
+            messages.error(request, "Usuário ou senha inválidos!")
+
+    return redirect('/')
+
+
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     # pra um apenas ou select usuario
     # evento = Evento.objects.get(id=1)
@@ -10,5 +36,5 @@ def lista_eventos(request):
 
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
-    dados = {'eventos':evento}
+    dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
