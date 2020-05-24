9865d4c6322efe8c9edad343bf49d221c8494653
mdalmeida79 <marcos.almeida@taking.com.br>
2020-05-24 15:47:36 -0300

    adicionado tabela de eventos e configuracoes

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null = True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao =models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Nome que desejop que a tabela tenha (pode n ser informado)
+    class Meta:
+        db_table = 'evento'
+
+    # as linhas abaixo server para definir qual campo deve aparecer
+    # na listagem de pesquisa
+    def __str__(self):
+        return self.titulo
